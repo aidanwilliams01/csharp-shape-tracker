dae642f56c39a481da1d04348c9feeee847ba82a
Aidan Williams <aidanwilliams01@gmail.com>
2023-09-08 15:25:06 -0700

    add passing tests, code for side 2

--- ShapeTracker.Tests/ModelTests/RectangleTests.cs
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -10,7 +10,7 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void RectangleConstructor_CreatesInstanceOfRectangle_Rectangle()
     {
-      Rectangle newRectangle = new Rectangle(3);
+      Rectangle newRectangle = new Rectangle(3, 2);
       Assert.AreEqual(typeof(Rectangle), newRectangle.GetType());
     }
 
@@ -18,7 +18,7 @@ namespace ShapeTracker.Tests
     public void GetSide1_ReturnSide1_Int()
     {
       int length1 = 3;
-      Rectangle newRectangle = new Rectangle(length1);
+      Rectangle newRectangle = new Rectangle(length1, 2);
       int result = newRectangle.Side1;
       Assert.AreEqual(length1, result);
     }
@@ -26,10 +26,28 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void SetSide1_SetsValueOfSide1_Void()
     {
-      Rectangle newRectangle = new Rectangle(3);
+      Rectangle newRectangle = new Rectangle(3, 3);
       int newLength1 = 44;
       newRectangle.Side1 = newLength1;
       Assert.AreEqual(newLength1, newRectangle.Side1);
+    }
+
+    [TestMethod]
+    public void GetSide2_ReturnsSide2_Int()
+    {
+      int length2 = 3;
+      Rectangle newRectangle = new Rectangle(2, length2);
+      int result = newRectangle.Side2;
+      Assert.AreEqual(length2, result);
+    }
+
+    [TestMethod]
+    public void SetSide2_SetsValueOfSide2_Void()
+    {
+      Rectangle newRectangle = new Rectangle(3, 4);
+      int newLength2 = 6;
+      newRectangle.Side2 = newLength2;
+      Assert.AreEqual(newLength2, newRectangle.Side2);
     }
   }
 }
