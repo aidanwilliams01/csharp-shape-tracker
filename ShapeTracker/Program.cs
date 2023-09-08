f23e4eb777517bb0ee4fa7917c2db64f0a9d83ba
Aidan Williams <aidanwilliams01@gmail.com>
2023-09-08 16:28:01 -0700

    add initial rectangle UI logic

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -10,48 +10,34 @@ namespace ShapeTracker
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Shape Tracker app!");
-      Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
-      Console.WriteLine("Please enter a number:");
-      string stringNumber1 = Console.ReadLine();  
-      Console.WriteLine("Enter another number:");
-      string stringNumber2 = Console.ReadLine();  
-      Console.WriteLine("Enter a third number:");
-      string stringNumber3 = Console.ReadLine();  
-      int length1 = int.Parse(stringNumber1);  
-      int length2 = int.Parse(stringNumber2);  
-      int length3 = int.Parse(stringNumber3);  
-      Triangle tri = new Triangle(length1, length2, length3);
-      ConfirmOrEditTriangle(tri);
-    }
-
-    static void CheckTriangles()
-    {
-      List<Triangle> allTriangles = Triangle.GetAll();
-      int triangle = 1;
-      Console.WriteLine("----------------------------------");
-      foreach (Triangle tri in allTriangles)
+      Console.WriteLine("Type 'triangle' to check the type of a triangle or 'rectangle' to calculate the area of a rectangle.");
+      string userResponse = Console.ReadLine();
+      if (userResponse == "triangle" || userResponse == "Triangle")
       {
-        Console.WriteLine($"Triangle {triangle}:");
-        Console.WriteLine($"Side one of the triangle: {tri.Side1}");
-        Console.WriteLine($"Side two of the triangle: {tri.Side2}");
-        Console.WriteLine($"Side three of the triangle: {tri.GetSide3()}");
-        Console.WriteLine("----------------------------------");
-        triangle += 1;
-      }
-      Console.WriteLine("Please enter 'new' to check the type of a new triangle, 'clear' to clear all triangles and return to the beginning of the program, or any key to exit.");
-      string userResponse = Console.ReadLine(); 
-      if (userResponse == "new" || userResponse == "New")
-      {
-        Main();
-      }
-      else if (userResponse == "clear" || userResponse == "Clear")
-      {
-        Triangle.ClearAll();
-        Main();
+        Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
+        Console.WriteLine("Please enter a number:");
+        string stringNumber1 = Console.ReadLine();  
+        Console.WriteLine("Enter another number:");
+        string stringNumber2 = Console.ReadLine();  
+        Console.WriteLine("Enter a third number:");
+        string stringNumber3 = Console.ReadLine();  
+        int length1 = int.Parse(stringNumber1);  
+        int length2 = int.Parse(stringNumber2);  
+        int length3 = int.Parse(stringNumber3);  
+        Triangle tri = new Triangle(length1, length2, length3);
+        ConfirmOrEditTriangle(tri);
       }
       else
       {
-        Console.WriteLine("Goodbye!");
+        Console.WriteLine("We'll calculate the area of your rectangle by multiplying the lengths of two sides.");
+        Console.WriteLine("Please enter a number:");
+        string stringNumber1 = Console.ReadLine();  
+        Console.WriteLine("Enter another number:");
+        string stringNumber2 = Console.ReadLine(); 
+        int length1 = int.Parse(stringNumber1);  
+        int length2 = int.Parse(stringNumber2);  
+        Rectangle rec = new Rectangle(length1, length2);
+        CalculateRectangleArea(rec);
       }
     }
 
@@ -100,6 +86,57 @@ namespace ShapeTracker
       else if (userResponse == "triangles" || userResponse == "Triangles")
       {
         CheckTriangles();
+      }
+      else
+      {
+        Console.WriteLine("Goodbye!");
+      }
+    }
+
+    static void CalculateRectangleArea(Rectangle rec)
+    {
+      float result = rec.CalculateArea();
+      Console.WriteLine("-----------------------------------------");
+      Console.WriteLine("Your result is: " + result + ".");
+      Console.WriteLine("-----------------------------------------");
+      Console.WriteLine("What's next?");
+      Console.WriteLine("Would you like to calculate the area of a new rectangle (new)?");
+      Console.WriteLine("Please enter 'new' to calculate the area of a new rectangle or any key to exit.");
+      string userResponse = Console.ReadLine(); 
+      if (userResponse == "new" || userResponse == "New")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Goodbye!");
+      }
+    }
+
+    static void CheckTriangles()
+    {
+      List<Triangle> allTriangles = Triangle.GetAll();
+      int triangle = 1;
+      Console.WriteLine("----------------------------------");
+      foreach (Triangle tri in allTriangles)
+      {
+        Console.WriteLine($"Triangle {triangle}:");
+        Console.WriteLine($"Side one of the triangle: {tri.Side1}");
+        Console.WriteLine($"Side two of the triangle: {tri.Side2}");
+        Console.WriteLine($"Side three of the triangle: {tri.GetSide3()}");
+        Console.WriteLine("----------------------------------");
+        triangle += 1;
+      }
+      Console.WriteLine("Please enter 'new' to check the type of a new triangle, 'clear' to clear all triangles and return to the beginning of the program, or any key to exit.");
+      string userResponse = Console.ReadLine(); 
+      if (userResponse == "new" || userResponse == "New")
+      {
+        Main();
+      }
+      else if (userResponse == "clear" || userResponse == "Clear")
+      {
+        Triangle.ClearAll();
+        Main();
       }
       else
       {
